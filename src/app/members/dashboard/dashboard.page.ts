751512c8c576b127aabcb86a38124f47ec23ce36
nwill30 <techexpressnw@gmail.com>
2019-01-08 21:37:06 -0600

    Create Login & Auth

---
+++ src/app/members/dashboard/dashboard.page.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.page.html',
+  styleUrls: ['./dashboard.page.scss'],
+})
+export class DashboardPage implements OnInit {
+
+  constructor(private authService: AuthenticationService) { }
+
+  ngOnInit() {
+  }
+
+  logout() {
+    this.authService.logout();
+  }
+}
---
+++ src/app/members/member-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const routes: Routes = [
+  { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardPageModule'}
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
+})
+export class MemberRoutingModule { }
